cd2f5cbb3ff944a44ed52277c48819c4683aa216
nihal <nihalmurali047@gmail.com>
2025-07-05 18:13:23 +0530

    implemented strategy to generate the departmentID automatically

--- ems/src/main/java/com/retailcloud/ems/Entity/DepartmentDetails.java
+++ ems/src/main/java/com/retailcloud/ems/Entity/DepartmentDetails.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +19,7 @@ import lombok.ToString;
 @Data
 public class DepartmentDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int departmentID;
 
     @Column(nullable = false, unique = true)
