5b52aab2d8e568f9871df2d2aa087d19f4983002
nihal <nihalmurali047@gmail.com>
2025-07-06 01:46:05 +0530

    API testing passed for save, failed for list and edit operations on Employee

--- ems/src/main/java/com/retailcloud/ems/Controller/EmployeeController.java
+++ ems/src/main/java/com/retailcloud/ems/Controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.retailcloud.ems.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.retailcloud.ems.Entity.EmployeeDetails;
 import com.retailcloud.ems.Service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 public class EmployeeController {
@@ -19,4 +26,15 @@ public class EmployeeController {
     {
         return empService.saveEmployee(empDet);
     }
+
+    @GetMapping("/listEmp")
+    public List<EmployeeDetails> listEmp() {
+        return empService.listEmp();
+    }
+
+    @PutMapping("editEmp/{id}")
+    public EmployeeDetails editEmp(@PathVariable Integer id, @RequestBody EmployeeDetails empDet) {
+        return empService.updateEmp(id, empDet);
+    }
+    
 }
