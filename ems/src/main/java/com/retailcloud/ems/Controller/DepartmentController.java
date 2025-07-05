70252b42a154a67ebb098d4d2bcdf9639bde8800
nihal <nihalmurali047@gmail.com>
2025-07-06 01:04:00 +0530

    API testing completed for save,list,delete and edit operations of department

--- ems/src/main/java/com/retailcloud/ems/Controller/DepartmentController.java
+++ ems/src/main/java/com/retailcloud/ems/Controller/DepartmentController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.retailcloud.ems.Entity.DepartmentDetails;
 import com.retailcloud.ems.Service.DepartmentService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -20,6 +26,23 @@ DepartmentService depService;
     public DepartmentDetails saveDept(@RequestBody DepartmentDetails saveDept)
     {     
         return depService.saveDept(saveDept);
+    }
+
+    @GetMapping("/listDept")
+    public List<DepartmentDetails> listDept()
+    {
+        return depService.listDept();
+    }
+    
+    @DeleteMapping("/deleteDept/{id}")
+    public void deleteDept(@PathVariable Integer id){
+        depService.deleteDept(id);
+    }
+
+    @PutMapping("/editDept/{id}")
+    public DepartmentDetails editDept(@PathVariable Integer id, @RequestBody DepartmentDetails deptDet)
+    {
+        return depService.editDept(id, deptDet);
     }
     
 }
