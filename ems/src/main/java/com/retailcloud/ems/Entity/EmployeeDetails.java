40e04ae97939ad04b7cc4a1c198d2171767ca5c1
nihal <nihalmurali047@gmail.com>
2025-07-05 18:30:40 +0530

    Changed the datatype of primary key from int to Integer, created a foreign key mapping to DepartmentDetails table, corrected the camelcase for reportingManager.

--- ems/src/main/java/com/retailcloud/ems/Entity/EmployeeDetails.java
+++ ems/src/main/java/com/retailcloud/ems/Entity/EmployeeDetails.java
@@ -5,8 +5,13 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,17 +22,21 @@ import lombok.NoArgsConstructor;
 @Data
 public class EmployeeDetails {
     @Id
-    @GeneratedValue
-    private int empId;    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer empId;    //best practive to use Integer instead of int for PKs
     private String name;  
     private LocalDate dob;
     @Column(precision = 12, scale = 2)
     private BigDecimal salary;
-    private int deptId;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false, foreignKey = @ForeignKey(name = "fk_employee_department"))
+    private DepartmentDetails department;
+
     private String address;
     private String role;
     private LocalDate joiningDate;
     @Column(precision = 5, scale = 2)
     private BigDecimal yearlyBonusPercent;
-    private String ReportingManager;
+    private String reportingManager;
 }
