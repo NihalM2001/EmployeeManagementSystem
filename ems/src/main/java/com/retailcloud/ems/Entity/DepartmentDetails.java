493f43e4ab5acff07aaa52bf6c30443bbb696625
nihal <nihalmurali047@gmail.com>
2025-07-06 20:47:27 +0530

    Declared a list object of EmployeeDetails type in DepartmentDetails entity to facilitate join query in DepartmentRepository

--- ems/src/main/java/com/retailcloud/ems/Entity/DepartmentDetails.java
+++ ems/src/main/java/com/retailcloud/ems/Entity/DepartmentDetails.java
@@ -1,12 +1,17 @@
 package com.retailcloud.ems.Entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,4 +32,7 @@ public class DepartmentDetails {
 
     private LocalDate creationDate;
     private String deptHead;
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EmployeeDetails> employees = new ArrayList<>();        //enables the join query in DepartmentRepository something to JOIN on
 }
