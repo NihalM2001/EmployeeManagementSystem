e03ec72930fead656c317c58f54fe3beb83366ae
nihal <nihalmurali047@gmail.com>
2025-07-06 21:26:34 +0530

    Created an Optional funcction of type DepartmentDetails for implementing the query of displaying employees of departments

--- ems/src/main/java/com/retailcloud/ems/Repository/DepartmentRepository.java
+++ ems/src/main/java/com/retailcloud/ems/Repository/DepartmentRepository.java
@@ -1,9 +1,17 @@
 package com.retailcloud.ems.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.retailcloud.ems.Entity.DepartmentDetails;
 
 public interface DepartmentRepository extends JpaRepository<DepartmentDetails, Integer>{
 
+    @Query("""
+        select d from DepartmentDetails d left join fetch d.employees where d.departmentID = :id
+            """)
+    Optional<DepartmentDetails> findNyIdWithEmployees(@Param("id") Integer id);
 }
